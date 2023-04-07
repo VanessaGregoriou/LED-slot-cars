4fe7f319c646ebcf07ddd8ca3a1cfc1c557bdd11
Steph OHara <soh_la@yahoo.com>
2023-04-07 11:43:41 +1000

    refactor of racer location code using pointer : corners logic using corner array : building with audio lib. but board crashes

--- LED_SlotCars_TOM/src/SoundEngine_VS1053.cpp
+++ LED_SlotCars_TOM/src/SoundEngine_VS1053.cpp
@@ -9,22 +9,23 @@ SoundEngine_VS1053::SoundEngine_VS1053(){
 void SoundEngine_VS1053::begin(void){
 
     if (! musicPlayer->begin()) { 
-        Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
-        while (1);
+      Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
+      while (1);
     }
     Serial.println(F("VS1053 found"));
 
     if (!SD.begin(CARDCS)) {
-    Serial.println(F("SD failed, or not present"));
-    while (1);  // don't do anything more
+      Serial.println(F("SD failed, or not present"));
+      while (1);  
     }
+    Serial.println(F("SD found"));
 
+    if(musicPlayer->useInterrupt(VS1053_FILEPLAYER_PIN_INT)){
+      Serial.print("interupt working");
+    };  
+    
     // make as loud as possible
     musicPlayer->setVolume(0,0);
-
-  if(musicPlayer->useInterrupt(VS1053_FILEPLAYER_PIN_INT)){
-    Serial.print("interupt working");
-  };  
 }
 
 void SoundEngine_VS1053::playSoundWithIndex(int i){
